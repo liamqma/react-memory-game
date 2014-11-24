9aae51a5efd67c2de2e7cab74d9d3501ea75e38e
Liam Ma <liam.ma@mi9.com.au>
2014-11-24 13:37:11 +1100

    Use server.coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+var del = require('del');
 
 gulp.task('default', function () {
 
     gulp.src(['./package.json', 'server.js', 'Procfile', 'public/*', 'public/**/*'], {base: './'})
         .pipe(gulp.dest('build'));
+
+
+    gulp.src('./server.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./build/'))
 
 });
---
+++ server.coffee
@@ -0,0 +1,19 @@
+finalhandler = require("finalhandler")
+http = require("http")
+serveStatic = require("serve-static")
+
+# Serve up public/ftp folder
+serve = serveStatic("public",
+    index: ["index.html"]
+)
+
+# Create server
+server = http.createServer((req, res) ->
+    done = finalhandler(req, res)
+    serve req, res, done
+    return
+)
+
+# Listen
+port = process.env.PORT or 3000
+server.listen port
